ef30e426fa2ccea8c75f8ea70f3fde7fe377d51b
alishashaw439 <alishashaw439@gmail.com>
2021-08-22 00:47:21 +0530

    leap year modified

--- java programs/Leap.java
+++ java programs/Leap.java
@@ -1,11 +1,20 @@
 public class Leap {
     public static void main(String args[]){
-        int n = 2028;
-        if((n % 400 == 0) || (n % 4 == 0) && (n % 100 !=0)){
-            System.out.println("Leap year");
-        }else{
-            System.out.println("Not a Leap year");
-        }
+        int n = 2014;
+      
+        boolean ans = isLeap(n);
+        System.out.println(ans);
 
+    }
+    public static boolean isLeap(int num){
+
+        if(num % 400 == 0){
+            return true;
+        }
+        if(num % 4 == 0 && num % 100 !=0){
+            return true;
+        }
+        return false;
+       
     }
 }
