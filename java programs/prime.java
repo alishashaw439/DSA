63846857176d495667426a900f2710a188953bc8
Alisha Shaw <alishashaw439@gmail.com>
2021-08-21 22:05:23 +0530

    prime

---
+++ java programs/prime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Prime{
+    public static void main(String args[] ) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        in.close();
+        if(num % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
