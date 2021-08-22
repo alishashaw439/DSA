80045f97430b1d8ebded485a14a4eecde435f720
alishashaw439 <alishashaw439@gmail.com>
2021-08-22 19:48:29 +0530

    sum of all numbers

---
+++ java programs/SumOfAllNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class SumOfAllNumbers{
+    public static void main(String args[]){
+        int sum=0;
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+             while(input != 0){
+                    sum = sum + input;
+                    input = in.nextInt();
+             }
+             System.out.println(sum);
+             in.close();
+           
+    
+    }
+}
