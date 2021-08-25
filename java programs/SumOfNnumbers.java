e3437710c23686742f3c85980eca68d270c17724
alishashaw439 <alishashaw439@gmail.com>
2021-08-25 23:44:03 +0530

    sum of N numbers

---
+++ java programs/SumOfNnumbers.java
@@ -0,0 +1,10 @@
+public class SumOfNnumbers {
+    public static void main(String [] args){
+        int num = 50;
+        int sum =0;
+        for(int i=0;i<num;i++){
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
