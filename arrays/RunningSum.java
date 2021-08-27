e74bba679be1ccb10bc9e999dfa02a344b4754ae
alishashaw439 <alishashaw439@gmail.com>
2021-08-27 22:25:58 +0530

    reduce memory in running array

--- arrays/RunningSum.java
+++ arrays/RunningSum.java
@@ -6,12 +6,19 @@ public class RunningSum {
             System.out.print(Arrays.toString(runningSum(nums)));
     }
     public static int[] runningSum(int[] nums) {
-        int[] ans = new int[nums.length];
-        ans[0] = nums[0];
+        //method 1
+        // int[] ans = new int[nums.length];
+        // ans[0] = nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     ans[i] = ans[i-1] + nums[i];
+        // }
+        // return ans;
+
+        //method 2, O(1)
         for(int i=1;i<nums.length;i++){
-            ans[i] = ans[i-1] + nums[i];
+            nums[i] += nums[i-1];
         }
-        return ans;
+        return nums;
     }
 
 }
