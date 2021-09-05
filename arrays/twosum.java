2e9b364eb3346392a500ed5332c4726cd510db05
alishashaw439 <alishashaw439@gmail.com>
2021-09-05 18:53:50 +0530

    Two sum array

---
+++ arrays/twosum.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+//https://leetcode.com/problems/two-sum/
+
+public class twosum {
+    public static void main(String args[]){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        //         int[] result = new int[2];
+        //         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        //         for(int i=0;i<nums.length;i++){
+        //             if(map.containsKey(target-nums[i])){
+        //                 result[1]=i;
+        //                 result[0]=map.get(target-nums[i]);
+        //                 return result;
+        //             }
+        //                 map.put(nums[i],i);
+                
+        //         }
+        //         return result;
+                
+                int[] result = new int[2];
+                for(int i=0;i<nums.length;i++){
+                    for(int j=i+1;j<nums.length;j++){
+                        if((nums[i] + nums[j]) == target){
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+                    
+                }
+                return result;
+            }
+    
+}
