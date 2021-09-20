ff80f22ac744d78c2479773d7dbb585311b5fd77
alishashaw439 <alishashaw439@gmail.com>
2021-09-21 00:00:36 +0530

    Jump game

---
+++ arrays/JumpGame.java
@@ -0,0 +1,19 @@
+public class JumpGame {
+
+    // https://leetcode.com/problems/jump-game/
+
+    public static void main(String args[]) {
+        int[] nums = { 2, 3, 1, 1, 4 };
+        System.out.println(canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+        int lastIndex = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastIndex) {
+                lastIndex = i;
+            }
+        }
+        return lastIndex == 0;
+    }
+}
