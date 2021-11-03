1a23ab3a7dd25f3029721db06fde4e1c3e8ca25b
alishashaw439 <alishashaw439@gmail.com>
2021-11-03 19:18:47 +0530

    kth positive integer java

---
+++ searching/kthpositiveInteger.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class kthpositiveInteger {
+    
+    public static void main(String args[]){
+       int[] array ={2,3,4,7,11};
+       int k = 5;
+       System.out.println(findKthPositive(array, k));
+    }
+    public static int findKthPositive(int[] arr, int k) {
+        List<Integer> list = new ArrayList<>();
+        int counter = 1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == counter){
+                counter++;
+            }else{
+                list.add(counter);
+                counter++;
+                i--;
+            }
+        }
+        System.out.println(list);
+        if(k > list.size()){
+            return arr[arr.length - 1] + (k - list.size());
+        }else{
+            return list.get(k-1);
+        }
+    
+        
+    }
+}
